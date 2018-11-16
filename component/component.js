c078e518a050986db088166848f53ae48e524b07
Westly Wright <westly.wright@gmail.com>
2018-11-16 15:44:24 -0700

    Inital Commit

---
+++ component/component.js
@@ -0,0 +1,101 @@
+/*!!!!!!!!!!!Do not change anything between here (the DRIVERNAME placeholder will be automatically replaced at buildtime)!!!!!!!!!!!*/
+// https://github.com/rancher/ui/blob/master/lib/shared/addon/mixins/cluster-driver.js
+import ClusterDriver from 'shared/mixins/cluster-driver';
+
+// do not remove LAYOUT, it is replaced at build time with a base64 representation of the template of the hbs template
+// we do this to avoid converting template to a js file that returns a string and the cors issues that would come along with that
+const LAYOUT;
+/*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
+
+
+/*!!!!!!!!!!!GLOBAL CONST START!!!!!!!!!!!*/
+// EMBER API Access - if you need access to any of the Ember API's add them here in the same manner rather then import them via modules, since the dependencies exist in rancher we dont want to expor the modules in the amd def
+const computed     = Ember.computed;
+const get          = Ember.get;
+const set          = Ember.set;
+const alias        = Ember.computed.alias;
+const service      = Ember.inject.service;
+
+/*!!!!!!!!!!!GLOBAL CONST END!!!!!!!!!!!*/
+
+
+
+/*!!!!!!!!!!!DO NOT CHANGE START!!!!!!!!!!!*/
+export default Ember.Component.extend(ClusterDriver, {
+  driverName:  '%%DRIVERNAME%%',
+  configField: '%%DRIVERNAME%%EngineConfig', // 'googleKubernetesEngineConfig'
+  app:         service(),
+  router:      service(),
+
+  init() {
+    // This does on the fly template compiling, if you mess with this :cry:
+    const decodedLayout = window.atob(LAYOUT);
+    const template      = Ember.HTMLBars.compile(decodedLayout, {
+      moduleName: 'shared/components/cluster-driver/driver-%%DRIVERNAME%%/template'
+    });
+    set(this,'layout', template);
+
+    this._super(...arguments);
+
+  },
+
+  config: computed('configField', function() {
+    const field = `cluster.${  get(this, 'configField') }`;
+
+    return get(this, field);
+  }),
+  /*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
+
+  // Write your component here, starting with setting 'model' to a cluster with your config populated
+  bootstrap: function() {
+    // bootstrap is called by rancher ui on 'init', you're better off doing your setup here rather then the init function to ensure everything is setup correctly
+    let config      = get(this, 'config');
+    let configField = get(this, 'configField');
+
+    if ( !config ) {
+      config = this.get('globalStore').createRecord({
+        type: configField,
+      });
+    }
+
+    set(this, `cluster.${ configField }`, config);
+  },
+
+  actions: {
+    save() {},
+    cancel(){
+      // probably should not remove this as its what every other driver uses to get back
+      get(this, 'router').transitionTo('global-admin.clusters.index');
+    },
+  },
+
+
+  // Add custom validation beyond what can be done from the config API schema
+  validate() {
+    // Get generic API validation errors
+    this._super();
+    var errors = get(this, 'errors')||[];
+    if ( !get(this, 'model.name') ) {
+      errors.push('Name is required');
+    }
+
+    // Add more specific errors
+
+    // Check something and add an error entry if it fails:
+    // if ( parseInt(get(this, 'config.memorySize'), defaultRadix) < defaultBase ) {
+    //   errors.push('Memory Size must be at least 1024 MB');
+    // }
+
+    // Set the array of errors for display,
+    // and return true if saving should continue.
+    if ( get(errors, 'length') ) {
+      set(this, 'errors', errors);
+      return false;
+    } else {
+      set(this, 'errors', null);
+      return true;
+    }
+  },
+
+  // Any computed properties or custom logic can go here
+});
---
+++ gulpfile.js
@@ -0,0 +1,137 @@
+/* jshint node: true */
+const gulp          = require('gulp');
+const clean         = require('gulp-clean');
+const gulpConcat    = require('gulp-concat');
+const gulpConnect   = require('gulp-connect');
+const replace       = require('gulp-replace');
+const babel         = require('gulp-babel');
+const argv          = require('yargs').argv;
+const pkg           = require('./package.json');
+const fs            = require('fs');
+const replaceString = require('replace-string');
+
+
+const NAME_TOKEN    = '%%DRIVERNAME%%';
+
+const BASE          = 'component/';
+const DIST          = 'dist/';
+const TMP           = 'tmp/';
+const ASSETS        = 'assets/';
+const DRIVER_NAME   = argv.name || pkg.name.replace(/^ui-driver-/,'');
+
+console.log('Driver Name:', DRIVER_NAME);
+
+if (!DRIVER_NAME) {
+  console.log('Please include a driver name with the --name flag');
+  process.exit(1);
+}
+
+gulp.task('default', ['build']);
+
+gulp.task('watch', function() {
+  gulp.watch([`./${ BASE }*.js`, `./${ BASE }*.hbs`, `./${ BASE }*.css`], ['build']);
+});
+
+gulp.task('clean', function() {
+  return gulp.src([`${DIST}*.js`, `${DIST}*.css`, `${DIST}*.hbs`, `${TMP}*.js`, `${TMP}*.css`, `${TMP}*.hbs`,], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('assets', ['styles'], function() {
+  return gulp.src(`${ ASSETS }*`)
+    .pipe(gulp.dest(DIST));
+});
+
+gulp.task('build', ['compile']);
+
+gulp.task('babel', ['assets'], function() {
+
+  const opts = {
+    'presets': [
+      ['env', {
+        'targets': {
+          'browsers': ['> 1%']
+        }
+      }]
+    ],
+    'plugins': [ 'add-module-exports',
+                 [ 'transform-es2015-modules-amd', {'noInterop': true} ]
+               ],
+    'moduleId': `shared/components/cluster-driver/driver-${ DRIVER_NAME }/component`,
+    'comments': false
+  };
+
+  let hbs = fs.readFileSync(`${BASE}template.hbs`, 'utf8');
+
+  hbs = replaceString(hbs, NAME_TOKEN, DRIVER_NAME);
+
+  hbs = Buffer.from(hbs).toString('base64');
+
+  return gulp.src([
+    `${BASE}component.js`
+  ])
+    .pipe(replace('const LAYOUT;', `const LAYOUT = '${ hbs }';`))
+    .pipe(replace(NAME_TOKEN, DRIVER_NAME))
+    .pipe(babel(opts))
+    .pipe(gulpConcat(`component.js`,{newLine: '\n;'}))
+    .pipe(gulp.dest(TMP));
+});
+
+gulp.task('rexport', ['babel'], function() {
+  const rexpOpts = {
+    'presets': [
+      ['env', {
+        'targets': {
+          'browsers': ['> 1%']
+        }
+      }]
+    ],
+    'plugins': [ 'add-module-exports',
+                 [ 'transform-es2015-modules-amd', { 'noInterop': true } ]
+               ],
+    'moduleId': `ui/components/cluster-driver/driver-${ DRIVER_NAME }/component`,
+  }
+
+  return gulp.src([
+    `${BASE}rexport.js`
+  ])
+    .pipe(replace(NAME_TOKEN, DRIVER_NAME))
+    .pipe(babel(rexpOpts))
+    .pipe(gulpConcat(`rexport.js`, {newLine: '\n;'}))
+    .pipe(gulp.dest(TMP));
+});
+
+gulp.task('alias', ['rexport'], function() {
+  return gulp.src([
+    `${BASE}alias.js`
+  ])
+    .pipe(replace(NAME_TOKEN, DRIVER_NAME))
+    .pipe(gulpConcat(`alias.js`, {newLine: '\n;'}))
+    .pipe(gulp.dest(TMP));
+});
+
+gulp.task('compile', ['alias'], function() {
+  return gulp.src([
+    `${TMP}component.js`,
+    `${TMP}rexport.js`,
+    `${TMP}alias.js`,
+  ])
+    .pipe(gulpConcat(`component.js`, {newLine: '\n;'}))
+    .pipe(gulp.dest(DIST));
+});
+
+
+gulp.task('styles', ['clean'], function() {
+  return gulp.src([`${ BASE }**.css`])
+    .pipe(replace(NAME_TOKEN, DRIVER_NAME))
+    .pipe(gulpConcat(`component.css`, {newLine: '\n;'}))
+    .pipe(gulp.dest(DIST));
+});
+
+gulp.task('server', ['build', 'watch'], function() {
+  return gulpConnect.server({
+    root: [DIST],
+    port: process.env.PORT || 3000,
+    https: false,
+  });
+});
